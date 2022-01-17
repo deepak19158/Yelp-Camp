8425f0bf5ceafd544eb0ccf475dd2b701cf07bf2
deepak19158 <80789344+deepak19158@users.noreply.github.com>
2022-01-17 22:14:48 +0530

    Add files via upload

---
+++ utils/ExpressError.js
@@ -0,0 +1,8 @@
+class ExpressError extends Error {
+    constructor(message, statuscode) {
+        super();
+        this.message = message;
+        this.statusCode = statuscode;
+    }
+}
+module.exports = ExpressError
